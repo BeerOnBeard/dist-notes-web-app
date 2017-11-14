7b2aca3bc70c4993e64bd4afc62b9af1596c47aa
BeerOnBeard <repoguy156+github@gmail.com>
2017-11-14 18:46:40 +0200

    Updated to NodeJS 8. Rewrote API to use async functions. Also, package-log.json is coming along for the ride due to the NPM 5 that was packaged with NodeJS 8.

--- NotesRepository.js
+++ NotesRepository.js
@@ -6,42 +6,37 @@ class NotesRepository {
     this.connectionString = connectionString;
   }
 
-  getNotesForUser(userId) {
-    return MongoClient.connect(this.connectionString)
-      .then(function(db){
-        return db.collection('notes').find({userId: userId}).toArray()
-          .then(function(items){
-            db.close();
-            return items;
-          });
-      });
+  async getNotesForUser(userId) {
+    var db = await MongoClient.connect(this.connectionString);
+    var items = await db.collection('notes')
+      .find({userId: userId})
+      .toArray();
+
+    db.close();
+    return items;
   }
 
-  addNoteForUser(userId, name, contents) {
-    return MongoClient.connect(this.connectionString)
-      .then(function(db){
-        return db.collection('notes').insert({
-          userId: userId,
-          name: name,
-          contents: contents
-        }).then(function(result){
-          db.close();
-          return result;
-        });
-      });
+  async addNoteForUser(userId, name, contents) {
+    var db = await MongoClient.connect(this.connectionString);
+    var result = await db.collection('notes').insert({
+      userId: userId,
+      name: name,
+      contents: contents
+    });
+
+    db.close()
+    return result;
   }
 
-  deleteNote(userId, noteId) {
-    return MongoClient.connect(this.connectionString)
-      .then(function(db){
-        return db.collection('notes').deleteOne({
-          _id: ObjectId(noteId),
-          userId: userId
-        }).then(function(result){
-          db.close();
-          return result;
-        });
-      });
+  async deleteNote(userId, noteId) {
+    var db = await MongoClient.connect(this.connectionString);
+    var result = await db.collection('notes').deleteOne({
+      _id: ObjectId(noteId),
+      userId: userId
+    });
+
+    db.close();
+    return result;
   }
 };
 
--- index.js
+++ index.js
@@ -8,41 +8,36 @@ const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/users/:userId/notes', function(req, res) {
+app.get('/users/:userId/notes', async(req, res) => {
   var userId = req.params.userId;
 
   var repo = new NotesRepository(config.mongodb);
-  var notes = repo.getNotesForUser(userId)
-    .then(function(items) {
-      res.json(items);
-    });
+  var notes = await repo.getNotesForUser(userId);
+  res.json(notes);
 });
 
-app.post('/users/:userId/notes', function(req, res) {
+app.post('/users/:userId/notes', async(req, res) => {
   var userId = req.params.userId;
   var note = req.body;
 
   var repo = new NotesRepository(config.mongodb);
-  repo.addNoteForUser(userId, note.name, note.contents)
-      .then(function(){
-        res.sendStatus(201);
-      });
+  var result = await repo.addNoteForUser(userId, note.name, note.contents);
+  res.sendStatus(201);
 });
 
-app.delete('/users/:userId/notes/:noteId', function(req, res) {
+app.delete('/users/:userId/notes/:noteId', async(req, res) => {
   var userId = req.params.userId;
   var noteId = req.params.noteId;
 
   var repo = new NotesRepository(config.mongodb);
-  repo.deleteNote(userId, noteId)
-      .then(function(result) {
-        if (result.deletedCount === 1) {
-          res.sendStatus(200);
-          return;
-        }
+  var result = await repo.deleteNote(userId, noteId);
 
-        res.sendStatus(404);
-      });
+  if (result.deletedCount === 1) {
+    res.sendStatus(200);
+    return;
+  }
+
+  res.sendStatus(404);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000.'));
